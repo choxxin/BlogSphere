ed31dfb8fdcd2b9b6edc37b804ac1bd6720d9b7b
ansh <kumaransh1975@gmail.com>
2024-06-11 22:29:21 +0530

    meow

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -5,9 +5,5 @@ const conf = {
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketd: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 };
-//console.log("APPWRITE_URL:", APPWRITE_URL);
-// console.log("APPWRITE_PROJECT_ID:", import.meta.env.VITE_PROJECT_ID);
-// console.log("APPWRITE_DATABASE_ID:", APPWRITE_DATABASE_ID);
-// console.log("APPWRITE_COLLECTION_ID:", APPWRITE_COLLECTION_ID);
-console.log("APPWRITE_BUCKET_ID:", import.meta.env.VITE_APPWRITE_BUCKET_ID);
+
 export default conf;
